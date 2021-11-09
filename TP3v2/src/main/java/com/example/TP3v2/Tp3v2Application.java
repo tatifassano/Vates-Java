20705898d3034a60fbdf10ed8432d6fefd281c5f
tatifassano <54126654+tatifassano@users.noreply.github.com>
2021-11-09 17:22:59 -0300

    Add files via upload

---
+++ TP3v2/src/main/java/com/example/TP3v2/Tp3v2Application.java
@@ -0,0 +1,13 @@
+package com.example.TP3v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3v2Application.class, args);
+	}
+
+}
---
+++ TP3v2/src/test/java/com/example/TP3v2/Tp3v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP3v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
