158e4480dae8b44a6a39c7185a2ee7ffc9682c9c
tatifassano <54126654+tatifassano@users.noreply.github.com>
2021-11-19 16:36:34 -0300

    Add files via upload

---
+++ equipos/src/main/java/prueba/equipos/EquiposApplication.java
@@ -0,0 +1,13 @@
+package prueba.equipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiposApplication.class, args);
+	}
+
+}
---
+++ equipos/src/test/java/prueba/equipos/EquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.equipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
